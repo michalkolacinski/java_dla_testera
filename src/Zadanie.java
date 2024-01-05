fd53f0f749fe02796b364c70f903abb13c4dc444
michalkolacinski <kolacinski.michal@gmail.com>
2024-01-05 15:13:53 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ src/Zadanie.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Zadanie {
+    public static void main(String[] args) {
+        String text = "developer";
+        String textNew = text.toUpperCase();
+        System.out.println(textNew);
+        System.out.println(text);
+
+
+
+
+    }
+}
