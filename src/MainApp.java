509daa99227f0bb0d6298e7bcc4e5f5107c00101
michalkolacinski <kolacinski.michal@gmail.com>
2024-01-05 09:44:34 +0100

    Merge remote-tracking branch 'origin/main'

--- src/MainApp.java
+++ src/MainApp.java
@@ -7,11 +7,17 @@ public class MainApp {
         //Start aplikacji
         System.out.println("hello from java 11");
         System.out.println(10);
+        //liczba całkowita
         System.out.println(10.5);
+        //liczba zmiennoprzecinkowa
         System.out.println("text");
+        //text
         System.out.println('s');
+        //znak
         System.out.println(true);
+        //true
         System.out.println(false);
+        //false
 
     }
 
