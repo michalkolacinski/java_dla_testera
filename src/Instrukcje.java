f8923ec41a7bb395cb162109ae76311d50e6dc6c
michalkolacinski <kolacinski.michal@gmail.com>
2024-12-11 22:24:29 +0100

    Merge remote-tracking branch 'origin/main'

--- src/Instrukcje.java
+++ src/Instrukcje.java
@@ -1,22 +1,25 @@
 package src;
 
 public class Instrukcje {
-    String homePage = "Home";
-    String loginPage = "Login";
-    String contactPage = "Contact";
 
-    String page = homePage;
-     switch (page){
-        case "Home":
-            System.out.println("przełączam się do strony Home" );
-            break;
-        case "Login":
-            System.out.println("przełączam się do strony Login" );
-            break;
-        case "Contact":
-            System.out.println("przełączam się do strony Contact" );
-            break;
-        default:
-            System.out.println("nie znam takiej strony");
+    public static void main(String[] args) {
+        String homePage = "Home";
+        String loginPage = "Login";
+        String contactPage = "Contact";
+
+        String page = homePage;
+        switch (page) {
+            case "Home":
+                System.out.println("przełączam się do strony Home");
+                break;
+            case "Login":
+                System.out.println("przełączam się do strony Login");
+                break;
+            case "Contact":
+                System.out.println("przełączam się do strony Contact");
+                break;
+            default:
+                System.out.println("nie znam takiej strony");
+        }
     }
 }
