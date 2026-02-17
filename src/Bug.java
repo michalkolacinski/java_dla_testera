73ca6a5fb3f42250057eccdaab0ce86f6325d70c
michalkolacinski <kolacinski.michal@gmail.com>
2026-02-17 23:40:49 +0100

    Merge remote-tracking branch 'origin/main'

--- src/Bug.java
+++ src/Bug.java
@@ -1,31 +1,35 @@
 package src;
+
 public class Bug {
     String bugDesciption;
     String reporterEmail;
     int bugPriority;
-    String bugStatus;
+    boolean bugStatus;
 
 
-    Bug (String bugDesciption, String reporterEmail, int bugPriority, String bugStatus){
-    this.bugDesciption = bugDesciption;
-    this.reporterEmail = reporterEmail;
-    this.bugPriority = bugPriority;
-    this.bugStatus = bugStatus;
+    Bug(String bugDesciption, String reporterEmail, int bugPriority, boolean bugStatus) {
+        this.bugDesciption = bugDesciption;
+        this.reporterEmail = reporterEmail;
+        this.bugPriority = bugPriority;
+        this.bugStatus = bugStatus;
 
     }
 
 
-    void getAllinfo(){
+    void showAllinfo() {
         System.out.println(bugDesciption + " " + reporterEmail + " " + bugPriority + " " + bugStatus);
     }
-    void showReporter(){
+
+    void showReporter() {
         System.out.println(reporterEmail);
     }
-    void showBugStatus(){
+
+    void showBugStatus() {
         System.out.println(bugStatus);
     }
-    void showBugPriority(){
-        System.out.println(bugPriority);
+
+    int getBugPriority() {
+        return bugPriority;
     }
 
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,16 +1,18 @@
 package src;
+
 public class MainApp {
     public static void main(String[] args) {
 
-        Bug bug1 = new Bug("login issue", "testowy@test.te", 1, "otwarty");
+        Bug bug1 = new Bug("login issue", "testowy@test.te", 1, false);
 
-        Bug bug2 = new Bug("button issue", "mail@mail.mail", 3, "zamkniety");
-        bug2.getAllinfo();
+        Bug bug2 = new Bug("button issue", "mail@mail.mail", 3,
+                true);
+        bug2.showAllinfo();
 
-        bug1.getAllinfo();
+        bug1.showAllinfo();
         bug1.showReporter();
         bug1.showBugStatus();
-        bug1.showBugPriority();
+        bug1.getBugPriority();
     }
 
 }
