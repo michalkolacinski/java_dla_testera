cdaf4e00428f385d1a46c4ab505deec2bf9a9857
michalkolacinski <kolacinski.michal@gmail.com>
2026-02-17 23:29:09 +0100

    Merge remote-tracking branch 'origin/main'

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,15 +2,15 @@ package src;
 public class MainApp {
     public static void main(String[] args) {
 
-        Bug bug = new Bug("login issue", "testowy@test.te", 1, "otwarty");
+        Bug bug1 = new Bug("login issue", "testowy@test.te", 1, "otwarty");
 
-        Bug bug1 = new Bug("button issue", "mail@mail.mail", 3, "zamkniety");
+        Bug bug2 = new Bug("button issue", "mail@mail.mail", 3, "zamkniety");
+        bug2.getAllinfo();
+
         bug1.getAllinfo();
-
-        bug.getAllinfo();
-        bug.showReporter();
-        bug.showBugStatus();
-        bug.showBugPriority();
+        bug1.showReporter();
+        bug1.showBugStatus();
+        bug1.showBugPriority();
     }
 
 }
